610cf626564cb7c2888a2d1a1c9751aa8bd8f764
strangeforloop <annaagoha@gmail.com>
2021-05-18 18:44:01 -0700

    escape quotes

--- src/components/withDimensionScreen.js
+++ src/components/withDimensionScreen.js
@@ -24,7 +24,7 @@ const StartScreen = ({setNewInstance, setDimension}) => {
   return (
     <ThemeProvider theme={theme}>
       <div style={{ 'maxWidth': '900px', 'margin': '3rem auto' }}>
-        <Typography variant="h4" style={{ 'marginBottom': '1rem'}}>Let&apos;'s play Tic Tac Toe or Connect Four</Typography>
+        <Typography variant="h4" style={{ 'marginBottom': '1rem'}}>Let&apos;s play Tic Tac Toe or Connect Four</Typography>
         {/* <Typography variant="h4" style={{ 'marginBottom': '1rem'}}>Configure your board!</Typography> */}
         <Typography variant="h5" color="primary">Enter board dimensions:</Typography>
         <Box style={{'marginTop': '1rem'}}>
